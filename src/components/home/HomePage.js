47c46b359f175e07ed1b6311c2b061b202134d84
Naren Vaishnavi <narenvaishnavi@users.noreply.github.com>
2018-02-06 22:59:31 -0500

    parallax-react

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { render } from 'react-dom';
+import { Parallax } from 'react-parallax';
 
-class HomePage extends React.Component{
-    render() {
-        return(
-            <div className="jumbotron">
-                <h1>ProLytics Sports Feed</h1>
-                <p>Are you a Sports freak ? You are at the right place</p>
-                <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+
+const styles = {
+    fontFamily: 'sans-serif',
+    textAlign: 'center'
+};
+const insideStyles = { background: 'white', padding: 10, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' };
+const image1 = "https://designmodo.com/wp-content/uploads/2013/08/MIO-Global.jpg";
+const image2 = "https://designmodo.com/wp-content/uploads/2013/08/MIO-Global.jpg";
+const image3 = "https://i.ytimg.com/vi/fvEBY1Zf70Y/maxresdefault.jpg";
+
+//
+const App = () => (
+    <div style={styles}>
+        <Parallax bgImage={image1}
+            strength={500}>
+            <div style={{ height: 700 }}>
+                <div style={insideStyles} >ProLytics is a multiple sports analytics suite used to perform advanced statistical
+analysis on professional and collegiate teams and athletes using machine learning and AI technologies.
+
+</div>
             </div>
-        );
-    }
-}
+        </Parallax>
+        <h1>| | |</h1>
+        <Parallax bgImage={image3} blur={{ min: -1, max: 3 }}>
+            <div style={{ height: 500 }}>
+                <div style={insideStyles}>For coaches, this analytics suite will allow them to analyze the team’s performance, a single player’s performance, their opponent’s tendencies and other strategic insights. For scouts, this analytics suite will provide in-depth look into prospective players and extract insights using predictive analytics.</div>
+            </div>
+        </Parallax>
+        <h1>| | |</h1>
+        <Parallax bgImage={image2} strength={-100}>
+            <div style={{ height: 700 }}>
+                <div style={insideStyles}>Reverse direction</div>
+            </div>
+        </Parallax>
+        <div style={{ height: 200 }}>
+        </div>
+        <h2>{'\u2728'}</h2>
+    </div>
+);
 
-export default HomePage;
+export default App;
